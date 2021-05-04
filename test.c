625068a6f5bd095703ee76b7b80c5c2ccff477ff
Kristján Valur Jónsson <sweskman@gmail.com>
2021-05-04 23:18:03 +0000

    Fix valgrind test errors

--- test.c
+++ test.c
@@ -1566,7 +1566,11 @@ static void test_async(struct config config) {
         while(astest.connected == 0)
             redisPollTick(c, 0.1);
         assert(astest.connected == -1);
-        redisAsyncFree(c);
+        /*
+         * freeing should not be done, clearing should have happened.
+         *redisAsyncFree(c);
+         */
+        assert(astest.ac == NULL);
         test_cond(astest.connect_status == REDIS_ERR);
     } else
         test_skipped();
@@ -1586,7 +1590,7 @@ static void test_async(struct config config) {
         while(astest.connected == 0)
             redisPollTick(c, 0.1);
         assert(astest.connected == -1);
-        redisAsyncFree(c);
+        assert(astest.ac == NULL);
         test_cond(astest.connect_status == REDIS_ERR);
     } else
         test_skipped();
